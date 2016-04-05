5cf5b7176b52a446645182530ab84819245f8241
janardhan18abhiBhejo <janardhan.s@eduteks.com>
2016-04-05 16:03:37 +0530

    Launch modes
    understanding hw launch mode works in android

---
+++ app/src/main/java/com/android/janardhan/launchmodes1/SingleInstanceActivity.java
@@ -0,0 +1,41 @@
+package com.android.janardhan.launchmodes1;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class SingleInstanceActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        switch (viewId) {
+            case R.id.standardMode:
+                startActivity(new Intent(this, StandardModeActivity.class));
+                break;
+
+            case R.id.singleTop:
+                startActivity(new Intent(this, SingleTopActivity.class));
+                break;
+
+            case R.id.singleInstance:
+                startActivity(new Intent(this, SingleInstanceActivity.class));
+                break;
+
+            case R.id.singleTask:
+                startActivity(new Intent(this, SingleTaskActivity.class));
+                break;
+        }
+
+    }
+
+}
